bf4362978bcbda1122627102e788577c499d2a0c
jhoel <jhoelpsandoval3sa@gmail.com>
2026-04-09 16:42:46 -0400

    modificaion de main y nav

---
+++ frontend/js/app.js
@@ -0,0 +1,8 @@
+
+function mostrarvista(vista){
+document.getElementById("view-home").classList.remove("active");
+document.getElementById("view-menu").classList.remove("active");
+document.getElementById("view-contact").classList.remove("active");
+
+document.getElementById("view-" + vista).classList.add("active");
+}
